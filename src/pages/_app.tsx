53a8bc5765fb747dda1e46817fb8f7e1277681d9
root <ykpn10@gmail.com>
2026-01-19 17:56:01 +0900

    サインアップ、サインアウト機能の実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from '@chakra-ui/react'
 import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
 import { getApp } from 'firebase/app'
+import { AuthProvider } from '@src/feature/auth/provider/AuthProvider'
+import { Header } from '@src/component/Header/Header'
+
 
 initializeFirebaseApp()
 function MyApp({ Component, pageProps }: AppProps) {
   console.log(getApp())
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Header />
+        <Component {...pageProps} />
+      </AuthProvider>
     </ChakraProvider>
   )
 }
